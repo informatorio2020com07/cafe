9a511a703012c2c83f90b1c2b8fa2a11b5c811b6
PameVI <pamelanarelaisla@gmail.com>
2020-08-02 23:39:16 -0300

    quitar cafetera

--- cafe.py
+++ cafe.py
@@ -96,6 +96,17 @@ class Cafeteria:
             raise ValueError("No es una cafetera válida. Agregue una cafetera")
             return False
 
+    def quitar_cafetera(cafeteria):
+        print("Quitar cafetera".center(centrado, "-"))
+        print("")
+        cafetera=cafeteria.get_cafeteras()
+        mostrar_cafeteras_enumeradas(cafeteras)
+        borrar=int(input("Que cafetera desea borrar: "))
+        cafetera_borrar=cafeteras[borrar -1]
+        cafeteria.quitar_cafetera(cafetera_borrar)
+        input()
+        print(" ",cafetera_borrar.get_nombre(),cafetera_borrar.str_cant_sobre_capacidad())
+        
 
 
 
