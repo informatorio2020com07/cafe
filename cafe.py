df6d1748576c8c6c66474bc7929a4324ea92830d
unknown <rosanapalavecino372@gmail.com>
2020-08-16 20:07:19 -0300

    agregamos método set para cambiar nombres de los atributos

--- cafe.py
+++ cafe.py
@@ -95,16 +95,12 @@ class Cafetera:
         """Lo mismo que to_dict(), solo que respeta el nombre establecido en las especificaciones."""
         return self.to_dict()
 
-    def set_cafetera(self,nueva_capacidad, nueva_cant, nueva_marca, nuevo_modelo, nuevo_nombre_id):
-        self.__capacidad = nueva_capacidad
-        self.__cant = nueva_cant
-        self.__marca = nueva_marca
-        self.__modelo = nuevo_modelo
-        self.__nombre_id = nuevo_nombre_id
-        if self.__capacidad!=self.__capacidad:
-            self.__capacidad=self.__nueva_capacidad
-            return True
-        
+    def set_cafetera(self,capacidad,cant,marca,modelo,nombre_id):
+        self.__capacidad=capacidad
+        self.cant=cant
+        self.__marca=marca
+        self.__modelo=modelo
+        self.__nombre_id=nombre_id
 
 
 
